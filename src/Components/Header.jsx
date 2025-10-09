3fee74040666371147edfef165a4cd1fc688f078
adeeldotcom <adeelawan.dev@gmail.com>
2025-10-09 11:37:28 +0500

    Adding new Componets

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = ()=>{
+    return <div>
+        <h1>This is Header </h1>
+    </div>
+}
+//Named Export
+export const UCP = ()=>{
+return <h2
+>This is named export Components
+</h2>
+
+}
+export default Header
---
+++ src/Components/Main.jsx
@@ -0,0 +1,7 @@
+const Main = ()=>{
+    return <div>
+        <h1>This is Main </h1>
+    </div>
+}
+
+export default Main
